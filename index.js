3a7a80aecfb73760b98e74afc6749fc3fe470bc9
KBoeh <kboeh3@gmail.com>
2023-03-06 11:02:37 -0500

    routes created

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('server is created')
+    const url = req.url
+    let tableData = "<table border='1'><tr><th>Name</th><th>Height</th><th>Birth Year</th><th>Gender</th><th>URL</th></tr>"
+
+    if (url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<h1>HI</h1>");
+        res.end(`<img src="https://dummyimage.com/600x400/000/fff&text=Home">`);
+    }
+   
+    else if (url === '/list') {
+        fetch('https://swapi.dev/api/people')
+        .then(res => res.json())
+        .then(data => {
+            let results = data.results;
+            createData(results);
+            // console.log(results);
+            res.write(tableData);
+            res.end();
+        });
+        
+    }
+
+    else {
+        res.write('Page Not Found');
+        res.end();
+    }
+
+    function createData(data) {
+        data.forEach(element => {
+            tableData +=`<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td></tr>`;
+        })
+        tableData +=`</table>`;
+    }
 })
 
 const PORT = 4000;
